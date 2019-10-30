f92863cdd0d926c92cf276ad62e2ebf2b69ebfff
GSCrawley <gscrawley@gmail.com>
2019-10-29 18:04:44 -0700

    randomized lines

--- poetry.py
+++ poetry.py
@@ -26,10 +26,14 @@ def lines_printed_backwards(lines_list):
     '''this function takes in a list of strings containing the lines of your poem as arguments and
     will print the poem lines out in reverse with the line numbers reversed'''
     #reverse the lines_list
+
+    print()
+
     lines_list.reverse()
 #
 # for lines in poem:
-    print(lines_list)
+    print(lines_printed_numbered(lines_list))
+
 
     #TODO: use loop to print out items in lines_list
     pass
@@ -38,14 +42,18 @@ def lines_printed_random(lines_list):
     '''randomly select lines from a list of strings and print them out in random order'''
     import random
     random.shuffle(lines_list)
-    print(lines_list)
+    print(lines_printed_numbered(lines_list))
 
-def my_custom_function():
-    '''does something of my choosing TODO: comment later'''
+
+def lines_printed_numbered(lines_list):
+    '''prints a numbered list'''
+    for index, value in enumerate(lines_list, 1):
+        print("{}. {}".format(index, value))
 
 
 #testing code
 #TODO: get poem string into list of lines
 lines_list = poem.split("\n")
+lines_printed_numbered(lines_list)
 lines_printed_backwards(lines_list)
 lines_printed_random(lines_list)
